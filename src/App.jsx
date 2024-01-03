bdd9207028f2d2da6edc2a9e87aedc76bb013caf
Natsumi.H <natsmy.1211@gmail.com>
2024-01-03 17:15:54 +0800

    🚀 Mantine setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import './App.css'
-
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css';
-
-import { MantineProvider } from '@mantine/core';
+import {
+  Anchor,
+  Button,
+  Container,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import classes from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <MantineProvider>(
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-  </MantineProvider>
+  return (
+    <Container size={420} my={40}>
+      <Title ta="center" className={classes.title}>
+        Welcome back!
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt={5}>
+        Do not have an account yet?{" "}
+        <Anchor size="sm" to="/signup">
+          Create account
+        </Anchor>
+      </Text>
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@mantine.dev" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+      </Paper>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "@mantine/core/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  fontFamily: "Montserrat, sans-serif",
+  defaultRadius: "md",
+  cursorType: "pointer",
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
