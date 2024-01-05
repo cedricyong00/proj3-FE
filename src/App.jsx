c6581ae10cdc40083390621007592b847df64144
cedricyong00 <cedricyong2000@gmail.com>
2024-01-05 15:21:15 +0800

    Pause Routing - Resuming later on at night

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,11 @@
-import {
-  Anchor,
-  Button,
-  Container,
-  Paper,
-  PasswordInput,
-  Text,
-  TextInput,
-  Title,
-} from "@mantine/core";
-import classes from "./App.module.css";
+import { Route, Routes } from "react-router-dom";
+import SignInPage from "./Sign-In page/Signin";
 
 function App() {
   return (
-    <Container size={420} my={40}>
-      <Title ta="center" className={classes.title}>
-        Welcome back!
-      </Title>
-      <Text c="dimmed" size="sm" ta="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Anchor size="sm" to="/signup">
-          Create account
-        </Anchor>
-      </Text>
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          required
-          mt="md"
-        />
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
-      </Paper>
-    </Container>
+    <Routes>
+      <Route path="/" element={<SignInPage />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "@mantine/core/styles.css";
 import { MantineProvider, createTheme } from "@mantine/core";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const theme = createTheme({
   fontFamily: "Montserrat, sans-serif",
@@ -13,8 +14,10 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Router>
     <MantineProvider theme={theme} defaultColorScheme="light">
       <App />
     </MantineProvider>
+    </Router>
   </React.StrictMode>
 );
