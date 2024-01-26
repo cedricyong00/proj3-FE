9f106ce979ab7f755d3fcee9fe649f4bea3b6558
cedricyong00 <cedricyong2000@gmail.com>
2024-01-27 02:39:46 +0800

    fixed logout

--- src/service/users.js
+++ src/service/users.js
@@ -7,3 +7,7 @@ export function getUser() {
   const token = getToken();
   return token ? JSON.parse(atob(token.split(".")[1])).payload : null;
 }
+
+export function logOut() {
+  localStorage.removeItem("token");
+}
